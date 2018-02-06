52011bdfa433ceefc2b2e6beca110aa57603d5c1
alexerate <35458285+alexerate@users.noreply.github.com>
2018-02-06 15:15:32 +0100

    localisation

--- interact.lua
+++ interact.lua
@@ -1,3 +1,5 @@
+local MP = minetest.get_modpath(minetest.get_current_modname())
+local S, NS = dofile(MP.."/intllib.lua")
 
 local old_is_protected = minetest.is_protected
 function minetest.is_protected(pos, name)
@@ -12,10 +14,8 @@ minetest.register_on_protection_violation(function(pos, name)
 	local playerpos = player:getpos()
 	if not areas:canInteract(pos, name) then
 		local owners = areas:getNodeOwners(pos)
-		minetest.chat_send_player(name,
-			("%s is protected by %s."):format(
-				minetest.pos_to_string(pos),
-				table.concat(owners, ", ")))
+		--minetest.chat_send_player(name,	("%s is protected by %s."):format(minetest.pos_to_string(pos), table.concat(owners, ", ")))
+		minetest.chat_send_player(name,S("@1 is protected by @2",minetest.pos_to_string(pos),table.concat(owners, ", ")))
 		minetest.after(1,anti_lag,{player=player,playerpos=playerpos})
 	end
 end)
