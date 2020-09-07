4018c0d20450a106b3bda6627894b130595a7cd6
Louis Royer <55180044+louisroyer@users.noreply.github.com>
2020-09-07 17:47:18 +0200

    Add privilege translations and update translations (#49)
    + Add new french translation strings

--- init.lua
+++ init.lua
@@ -21,18 +21,20 @@ dofile(areas.modpath.."/hud.lua")
 
 areas:load()
 
+local S = minetest.get_translator("areas")
+
 minetest.register_privilege("areas", {
-	description = "Can administer areas.",
+	description = S("Can administer areas."),
 	give_to_singleplayer = false
 })
 minetest.register_privilege("areas_high_limit", {
-	description = "Can protect more, bigger areas.",
+	description = S("Can protect more, bigger areas."),
 	give_to_singleplayer = false
 })
 
 if not minetest.registered_privileges[areas.config.self_protection_privilege] then
 	minetest.register_privilege(areas.config.self_protection_privilege, {
-		description = "Can protect areas.",
+		description = S("Can protect areas."),
 	})
 end
 
