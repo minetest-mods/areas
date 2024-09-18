9b6fea147308603d17397caddb3aa238fbcc5a9b
Luke aka SwissalpS <Luke@SwissalpS.ws>
2024-09-18 23:42:09 +0200

    fix trailing whitespace in translatable strings
    Good editors trim trailing whitespace from EOL.
    This means we need to avoid translatable strings that include spacers at
    the end. They need to be added in code.

--- pos.lua
+++ pos.lua
@@ -152,7 +152,7 @@ minetest.register_chatcommand("area_pos", {
 			areas.set_pos[name] = "pos2"
 			return true, S("Select position @1 by punching a node.", "2")
 		elseif param == "get" then -- Display current area positions
-			local pos1str, pos2str = S("Position @1: ", "1"), S("Position @1: ", "2")
+			local pos1str, pos2str = S("Position @1:", " 1"), S("Position @1:", " 2")
 			if areas.pos1[name] then
 				pos1str = pos1str..minetest.pos_to_string(areas.pos1[name])
 			else
